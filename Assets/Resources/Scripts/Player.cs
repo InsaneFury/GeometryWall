3139192c7478785195f68271c91196c5f0b5ac78
insanefury <justdarsan@gmail.com>
2019-05-17 03:36:58 -0300

    Added basics mechanics

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /* public Transform to;
+     //public Transform player;
+     public float rotationSpeed = 20f;
+     float rotateGrades = 90f;
+
+     private void Start()
+     {
+        to = this.transform;
+     }
+
+     void Update()
+     {
+
+         transform.rotation = Quaternion.RotateTowards(
+             transform.rotation,
+             to.rotation,
+             rotationSpeed * Time.deltaTime);
+
+         if (Input.GetKeyDown(KeyCode.A))
+         {
+             to.rotation = Quaternion.Euler(to.rotation.x, to.rotation.y + rotateGrades, to.rotation.z);
+             Debug.Log("Rotate Left");
+         }
+         if (Input.GetKeyDown(KeyCode.D))
+         {
+             Debug.Log("Rotate Right");
+         }
+
+     }*/
+
+    private Quaternion startingRotation;
+    public float speed = 10;
+    public float leftDegrees = -90f;
+    public float rightDegrees = 90f;
+
+    void Start()
+    {
+        //save the starting rotation
+        startingRotation = this.transform.rotation;
+    }
+
+    void Update()
+    {
+        //return back to the starting rotation
+        /*if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Rotate(0));
+        }*/
+
+        //go to 90 degrees with right arrow
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Rotate(rightDegrees));
+        }
+
+        //go to -90 degrees with left arrow
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Rotate(leftDegrees));
+        }
+
+    }
+
+    IEnumerator Rotate(float rotationAmount)
+    {
+        Quaternion finalRotation = Quaternion.Euler(0, this.transform.rotation.y + rotationAmount, 0) * startingRotation;
+
+        while (this.transform.rotation != finalRotation)
+        {
+            startingRotation = Quaternion.Euler(0, startingRotation.y + rotationAmount, 0);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, finalRotation, Time.deltaTime * speed);
+            yield return 0;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/WallSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    public List<GameObject> wallsSpawned;
+    public GameObject wall;
+    public int amountOfWalls = 2;
+    public Vector3 startPosition;
+    public Vector3 resetPosition;
+    public float spaceBetween = 40f;
+    public float moveSpeed = 1f;
+    void Start()
+    {
+        wallsSpawned = new List<GameObject>();
+        for (int i = 0; i < amountOfWalls; i++)
+        {
+            wallsSpawned.Add(Instantiate(wall,wall.transform.position,Quaternion.Euler(new Vector3(0f,90f,90f))));
+            wallsSpawned[i].SetActive(true);
+            if (i == 0)
+            {
+                wallsSpawned[i].transform.localPosition = startPosition;
+            }
+            else
+            {
+                wallsSpawned[i].transform.localPosition = new Vector3(wallsSpawned[i].transform.localPosition.x,
+                                                                 wallsSpawned[i].transform.localPosition.y,
+                                                                 wallsSpawned[i].transform.localPosition.z + spaceBetween);
+            }
+           
+        }
+    }
+
+    void Update()
+    {
+        foreach (GameObject go in wallsSpawned)
+        {
+            go.transform.Translate(Vector3.up * -moveSpeed * Time.deltaTime);
+            if (go.transform.position.z <= resetPosition.z)
+            {
+                go.transform.position = startPosition;
+            }
+        }
+    }
+}
